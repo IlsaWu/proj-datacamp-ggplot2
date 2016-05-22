ea4fdc762e2850b67dd22ba4b90efae33542da5d
jyuill <john@fig4.com>
2016-05-21 17:58:32 -0700

    ch3

---
+++ ggplot2-1-ch3-aesthetics.R
@@ -0,0 +1,16 @@
+### Datacamp: Data Visualization with Ggplot2 1, chapter 3 aesthetics
+## https://campus.datacamp.com/courses/data-visualization-with-ggplot2-1/chapter-3-aesthetics?ex=1 
+
+## Common aesthetics
+# x: x axis position
+# y: y axis position
+# color: color of dots, outlines of other shapes
+# fill: fill color
+# size: diameter of dots, thickness of lines
+# alpha: transparency (0=transparent, 1=opaque)
+# linetype: line dash pattern
+# labels: text on plot or axes
+# shape: shape of points
+
+
+
